e4c0dfea813dfc374a76a23e3fedbb8147b712d4
Andrew Yu <andrewzyu01@gmail.com>
2020-06-02 23:05:51 -0400

    basic search working

--- interface/server.js
+++ interface/server.js
@@ -14,14 +14,23 @@ MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology: true})
     app.set('view engine', 'pug')
 
     app.get('/', async (req, res) => {
-      sound.find().sort({'game_meta.sound_label': -1, 'meta.category': -1}).toArray()
-        .then(results => {
-          console.log(results)
-          res.render('dataset', {sounds: results})
-        })
+      const { q } = req.query
+      const results = await getResults(q)
+      
+      console.log(results)
+      res.render('dataset', {sounds: results})
     })
 
     app.listen(5000,() => console.log('listening on port 5000.'))
 
   })
   .catch(err => console.error(err))
+
+async function getResults (query) {
+  return sound.find({$or: [
+    {'game_meta.sound_label': query},
+    {'meta.category': query}
+  ]}).sort({
+    'game_meta.sound_label': -1, 
+    'meta.category': -1}).toArray()
+}
