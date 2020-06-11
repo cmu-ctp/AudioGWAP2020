32bde311e36b49f1fa137e6c3022b5597ca3667a
Andrew Yu <andrewzyu01@gmail.com>
2020-06-11 18:20:36 -0400

    Access Control Support

--- interface/server.js
+++ interface/server.js
@@ -1,27 +1,38 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
+require('dotenv').config()
 
 let sound
+let mongoURL = 'mongodb://'
+if (process.env.MONGO_USER && process.env.MONGO_PASS) {
+  const user = encodeURIComponent(process.env.MONGO_USER)
+  const pass = encodeURIComponent(process.env.MONGO_PASS)
+  mongoURL += `${user}:${pass}@`
+}
+mongoURL += `${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
+if (process.env.MONGO_AUTH_SOURCE) {
+  mongoURL += `?authSource=${process.env.MONGO_AUTH_SOURCE}`
+}
 
-MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology: true})
+
+MongoClient.connect(mongoURL, {useUnifiedTopology: true})
   .then(client => {
     console.log('Connected to MongoDB!')
-    sound = client.db('echoes').collection('sound')
+    sound = client.db(process.env.MONGO_DB).collection('sound')
 
     app.set('view engine', 'pug')
-      app.use(express.static('public'));
+    app.use(express.static('public'));
 
     app.use('/upload', express.static('../project/server/upload'))
 
     app.get('/', async (req, res) => {
       const { q } = req.query
-        let results = []
-        if(q != null) {
-            results = await getResults(q)
-        }
-      if(q) {
+      let results = []
+      if (q && q.length > 0) {
+          results = await getResults(q)
+      }
+      if (q) {
         console.log('Query: ' + q)
       }
       res.render('dataset', {sounds: results, query: q})
@@ -34,14 +45,10 @@ MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology: true})
 
 async function getResults (query) {
   let cursor
-  if (query && query.length > 0) {
-    cursor = await sound.find({$or: [
-      {'game_meta.sound_label': {$regex: query, $options: 'i'}},
-      {'meta.category': {$regex: query, $options: 'i'}}
-    ]})
-  } else {
-    cursor = await sound.find()
-  }
+  cursor = await sound.find({$or: [
+    {'game_meta.sound_label': {$regex: query, $options: 'i'}},
+    {'meta.category': {$regex: query, $options: 'i'}}
+  ]})
   return cursor.sort({
     'game_meta.sound_label': -1, 
     'meta.category': -1}).toArray()
