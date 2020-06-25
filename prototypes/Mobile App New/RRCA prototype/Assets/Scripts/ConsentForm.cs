21e7f6fa1aef00e56886a6f1aced9f56f303c5f4
blythe1215 <blythe1215@gmail.com>
2020-06-25 14:51:35 -0400

    Added checkboxes and complete consent form

---
+++ prototypes/Mobile App New/RRCA prototype/Assets/Scripts/ConsentForm.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsentForm : MonoBehaviour
+{
+    [SerializeField]
+    private Toggle checkbox1;
+    [SerializeField]
+    private Toggle checkbox2;
+    [SerializeField]
+    private Toggle checkbox3;
+
+    [SerializeField]
+    private Button acceptButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        acceptButton.gameObject.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (checkbox1.isOn && checkbox2.isOn && checkbox3.isOn) {
+            Debug.Log("toggle is on");
+            acceptButton.gameObject.SetActive(true);
+        }
+        else {
+            acceptButton.gameObject.SetActive(false);
+        }
+        
+    }
+}
