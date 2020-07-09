ef8a65ee0e46c7b2b62460311289d5364b8828ed
Andrew Yu <andrewzyu01@gmail.com>
2020-07-09 01:44:24 -0400

    removing unnecessary code

--- interface/public/script.js
+++ interface/public/script.js
@@ -8,24 +8,6 @@ $(".parent").click(function () {
     $(subClass).prop('checked', this.checked);
 })
 
-function check(categoryName) {
-    var allChecked = true;
-    if($("#"+categoryName)[0].checked){
-        $(".sub-"+categoryName).prop("checked", true);
-    } else {
-        $(".sub-"+categoryName).prop("checked", false);
-        $(".checkAll")[0].checked = false;
-    }
-    $('td input:checkbox').each(function () {
-        if(!this.checked) {
-            allChecked = false
-        }
-    })
-    if(allChecked) {
-        $(".checkAll")[0].checked = true;
-    }
-
-}
 function uncheck(data, categoryName) {
     var allSubChecked = true;
     var noneChecked = true;
