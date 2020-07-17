0013e5fd76823c187f177c353a87c38ed0403f8b
Andrew Yu <andrewzyu01@gmail.com>
2020-07-17 15:18:22 -0400

    Added admin panel react app, removed node_modules from git tracking

---
+++ interface/admin-panel/src/config/config.js
@@ -0,0 +1,12 @@
+export const CONFIG = {
+  uiUrl: 'http://localhost:3000',
+  serverIp: 'https://localhost/api',
+  userInfo: '/users/info',
+  authLogin: '/auth/login',
+  authLogout: '/auth/logout',
+  streamerEvents: '/streamer/events',
+  streamerUpcomingEvents: '/streamer/events/upcoming',
+  streamerPastEvents: '/streamer/events/past',
+  userStats: '/stat/users',
+  statCategories: '/stat/categories'    
+}
