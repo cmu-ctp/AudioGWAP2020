fc0c75e7858ead1bacf60ea2000cd2b6a628d57a
Andrew Yu <andrewzyu01@gmail.com>
2020-07-25 21:17:59 -0400

    Added api endpoints for adding, editing, and deleting categories

--- project/server/api/admin/categories.js
+++ project/server/api/admin/categories.js
@@ -5,24 +5,128 @@
 
 const Router = require('koa-router');
 const Category = require('../../models/sound_category');
+const Joi = require('@hapi/joi');
+const bodyParser = require('koa-body');
 
 const userAuth = require('../../lib/session').authRole;
 
 const router = new Router();
 
+const categorySchema = Joi.object({
+  parent: Joi.string().alphanum().allow('/', '-'),
+  sub: Joi.string().alphanum().allow('/', '-')
+}).min(2);
+
+const updateCategorySchema = Joi.object({
+  parent: Joi.string().alphanum().allow('/', '-'),
+  sub: Joi.string().alphanum().allow('/', '-')
+}).min(1);
+
 router.use(userAuth({ blockRequest: true, roleRequired: 1 }));
 
-//TODO: Add routes for editing categories (Add category, edit category)
-router.get('/', async (ctx) => {
-  const uid = ctx.user.uid;
+/**
+ * POST /admin/categories
+ * Add a new category to the list of categories
+ * 
+ * Send with url-encoded format with following parameters:
+ * parent='name of parent category'
+ * sub='name of subcategory'
+ */
+router.post('/categories', async (ctx) => {
+  // const uid = ctx.user.uid;
+  console.log(ctx.request.body);
+  let categoryData = Object.create(ctx.request.body);
 
-  const userModel = new User(ctx);
-  const userInfo = await userModel.findByUid(uid);
+  try {
+    categoryData = await categorySchema.validateAsync(categoryData);
+  } catch (err) {
+    ctx.throw(400, err);
+  }
 
-  ctx.body = {
-    'msg': 'Success',
-    'result': userInfo
-  };
+  console.log(categoryData);
+  
+  const CategoryModel = new Category(ctx);
+  try {
+    const idObj = await CategoryModel.addCategory(categoryData.parent, categoryData.sub)
+    //addCategory returns null if it already exists
+    if (!idObj) {
+      ctx.throw(400, 'Category already exists');
+    }
+    console.log('Successfully added!');
+    console.log(idObj);
+    ctx.body = {
+      'msg': 'Success',
+      'id': idObj.id
+    };
+  } catch (err) {
+    console.log(err);
+    ctx.throw(500, err);
+  }
 });
+
+/**
+ * PUT /admin/categories/:id
+ * Update the category designated by :id
+ * 
+ * Request should be in json form, as follows:
+ * {
+ *    parent: 'name of new parent'
+ *    sub: 'new subcategory name'
+ * }
+ * 
+ * If any fields aren't present, the end point will assume that field shouldn't be changed.
+ */
+router.put('/categories/:id', async (ctx) => {
+  const id = ctx.params.id;
+  console.log(ctx.request.body);
+  let categoryData = Object.create(ctx.request.body);
+  let CategoryModel = new Category(ctx);
+  const category = await CategoryModel.find(id);
+
+  if (category === null) {
+    ctx.throw(404, 'Category not found');
+  }
+
+  try {
+    categoryData = await updateCategorySchema.validateAsync(categoryData);
+  } catch (err) {
+    ctx.throw(400, err);
+  }
+
+  try {
+    await CategoryModel.update(id, categoryData);
+    ctx.body = {
+      'msg': 'Success'
+    }
+  } catch (err) {
+    console.log(err);
+    ctx.throw(500, err);
+  }
+})
+
+/**
+ * DELETE /admin/categories/:id
+ * Deletes the category designated by id
+ */
+router.delete('/categories/:id', async (ctx) => {
+  const id = ctx.params.id;
+  const CategoryModel = new Category(ctx);
+  const category = await CategoryModel.find(id);
+
+  if (category === null) {
+    ctx.throw(404, 'Category not found');
+  }
+
+  try {
+    await CategoryModel.remove(id);
+    ctx.body = {
+      'msg': 'Success',
+      'Removed Category': category.parent + '/' + category.sub
+    }
+  } catch (err) {
+    console.log(err);
+    ctx.throw(500, err);
+  }
+})
 
 module.exports = router;
