c03909c80f1d930e9fa292a2229ac6dd71fd8391
sanyaagrawal <agra.sanya@gmail.com>
2020-07-27 15:36:50 -0400

    fixed null reference error in rolling rhaposody game:

--- prototypes/RollingPrototype/Assets/Scripts/Recognize.cs
+++ prototypes/RollingPrototype/Assets/Scripts/Recognize.cs
@@ -29,6 +29,13 @@ public class Recognize : MonoBehaviour
     // of whose sound is being collected now.
     public IEnumerator ShowRecognization(string userName)
     {
+
+        recognizeText = GetComponent<Text>();
+        canvasGroup = GetComponent<CanvasGroup>();
+
+        Debug.Assert(recognizeText != null, gameObject.name + ": no recognizeText found");
+        Debug.Assert(canvasGroup != null, gameObject.name + ": no canvasGroup found");
+
         recognizeText.text = "Sound from <color=#F85797>" + userName + "</color> is collected!";
         float elapsedTime = 0f;
         while (elapsedTime < fadeTime)
