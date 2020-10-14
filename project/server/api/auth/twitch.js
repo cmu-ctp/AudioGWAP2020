215f0c83f8487bb5ff4648e2f3d0a480833d6671
Aishwarya Jaiswal <jaiswal.aishwarya95@gmail.com>
2020-10-13 21:20:08 -0400

    Added debug logs

--- project/server/api/auth/twitch.js
+++ project/server/api/auth/twitch.js
@@ -23,6 +23,7 @@ router.get('/auth/twitch/web', async (ctx) => {
   const tokenInfo = Object.create(null);
   tokenInfo.access_token = ctx.query.access_token || '';
   tokenInfo.refresh_token = ctx.query.refresh_token || '';
+  console.log('Tokens');
   console.log(tokenInfo.refresh_token);
   console.log(tokenInfo.access_token);
   if (!tokenInfo.access_token) {
